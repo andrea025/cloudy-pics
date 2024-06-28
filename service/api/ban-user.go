57e372fbb8f864e4bd14ece5517cd210ed8c2ea5
Chiara <chiara@email.com>
2024-06-28 03:56:42 -0400

    update Chiara

--- service/api/ban-user.go
+++ service/api/ban-user.go
@@ -14,7 +14,7 @@ func (rt *_router) banUser(w http.ResponseWriter, r *http.Request, ps httprouter
 
 	err := rt.db.BanUser(uid, tuid)
 
-	if errors.Is(err, database.ErrUserDoesNotExist) {
+	if errors.Is(err, database_nosql.ErrUserDoesNotExist) {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	} else if err != nil {
